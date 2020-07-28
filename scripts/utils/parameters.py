85acc6c53581b932341c780acd56f8c7548e343a
12rambau <pierrick.rambaud@orange.fr>
2020-07-28 14:45:00 +0200

    bloc 1

---
+++ scripts/utils/parameters.py
@@ -0,0 +1,13 @@
+
+###########################################
+###          annual time serie          ###
+###########################################
+
+def getHeader():
+    """return the expected header list"""
+    map_code = "map_class"
+    point_id = "id"
+    xcoord = "XCoordinate"
+    ycoord = "YCoordinate"
+    
+    return [map_code, point_id, xcoord, ycoord]
