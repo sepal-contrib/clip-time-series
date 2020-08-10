7e5ca65c3826981549206e0f8d649d9ec8f07346
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-08-10 14:19:33 +0000

    read the pts file

---
+++ utils/messages.py
@@ -0,0 +1,10 @@
+########################
+##    table loading   ##
+########################
+
+TABLE_INTRO = "Select the table in your sepal folders"
+TABLE_BTN = "load your pts file"
+FILE_SELECT = "Select a file"
+NOT_A_FILE = "the file {} does not exist"
+ERROR_READING_FILE = "the file {} cannot be read"
+WRONG_HEADERS = "The header of the file are not well diplayed"
