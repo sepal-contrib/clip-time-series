fb9ae5818f2d67a3a0e29a3912cd84e5bd2f94a6
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-08-12 10:13:52 +0000

    vizualization of interest points on static maps

--- utils/messages.py
+++ utils/messages.py
@@ -8,3 +8,17 @@ FILE_SELECT = "Select a file"
 NOT_A_FILE = "the file {} does not exist"
 ERROR_READING_FILE = "the file {} cannot be read"
 WRONG_HEADERS = "The header of the file are not well diplayed"
+
+
+##################################
+##         vizualize points     ##
+##################################
+
+VIZ_INTRO = "Select sources and bands to be displayed"
+VIZ_BTN = "Validate your bands"
+VIZ_SOURCES = "select the sources satellites"
+VIZ_BANDS = "select the band to be displayed"
+VIZ_POINT = "Display Point {}"
+NO_PTS = "No points of observation are registered. Please go back to step 1"
+NO_BANDS = "Please provide a the bands you want to observe on your images"
+NO_SOURCES = "please provide the satellite sources"
--- utils/parameters.py
+++ utils/parameters.py
@@ -4,10 +4,10 @@ import os
 import glob
 
 def getAvailableBands():
-    """give the bands composition for each name. 0 being the landsat compostiotn and 1 the sentinel"""
+    """give the bands composition for each name. 0 being the landsat composition and 1 the sentinel"""
     
     bands = {
-        'Red, Green, Blue' : [],
+        'Red, Green, Blue' : [['B3', 'B2', 'B1'], ['B4', 'B3', 'B2']],
         'Nir, Red, Green' : [],
         'Nir, Swir1, Red' : [],
         'Swir2, Nir, Red' : [],
@@ -19,7 +19,10 @@ def getAvailableBands():
 
 def getSources():
     
-    return ['landsat', 'sentinel']
+    return {
+        'landsat': 'LANDSAT/LE07/C01/T1_SR', 
+        'sentinel': 'COPERNICUS/S2_SR'
+    }
 
 def getTxt():
     """get all the txt files available in th folders"""
@@ -27,3 +30,24 @@ def getTxt():
     raw_list = glob.glob(root_dir + "/**/*.txt", recursive=True)
     
     return raw_list
+
+def sentinelVizParam(bands):
+    return {
+        'min': 0.0,
+        'max': 0.3,
+        'bands': bands
+    }
+
+def landsatVizParam(bands):
+    return {
+        'min': 0,
+        'max': 3000,
+        'bands': bands
+    }
+
+def getnbIntervals():
+    return sentinel_end - landsat_start + 1
+
+landsat_start = 2005
+sentinel_start = 2016
+sentinel_end = 2019
