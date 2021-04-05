210f63175a753a62441514e0a080ebcd656517f2
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-04-05 15:54:58 +0000

    implement the getdownloadurl

--- component/parameter/pdf.py
+++ component/parameter/pdf.py
@@ -1,7 +1,29 @@
-def getPositionPdf(i):  
-    """Return the position of the square on the pdf page"""
-    return [int(i/5), i%5]
+from itertools import product
 
-nb_line = 4
-nb_col = 5
+def get_dims(N):
+    '''
+    I'm gonna check every combination from 1 to 20 lines and columns.
+    400 year of data max, I'll have a good life before anyone complains
+    '''
+
+    # A4 format in landscape
+    width = 11.69
+    heigh = 8.27
+
+    cols, lines = (None, None)
+    l = 0
+    for nb_col, nb_line in product(range(1, 21), range(1, 21)):
+
+        l_tmp = min(width/nb_col, heigh/nb_line)
+
+        if l_tmp > l and nb_col*nb_line > N:
+            l = l_tmp
+            cols = nb_col
+            lines = nb_line
+
+        return (cols, lines)
+
+def getPositionPdf(i, nb_col):  
+    """Return the position of the square on the pdf page"""
+    return [int(i/nb_col), i%nb_col]
 
