0e6812b49c76916b5c99154d6780e568fe87385d
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-04-14 08:50:43 +0000

    add the planet imagery

--- component/scripts/planet.py
+++ component/scripts/planet.py
@@ -1,10 +1,19 @@
 # this file will be used as a singleton object in the explorer tile 
-
+from pathlib import Path
 import requests
 from types import SimpleNamespace
+from itertools import product
 
 from planet import api
 from ipyleaflet import TileLayer
+from shapely import geometry as sg
+from shapely.ops import unary_union
+from pyproj import CRS, Transformer
+import numpy as np
+import rasterio as rio
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+import geopandas as gpd
+from osgeo import gdal
 
 from component.message import cm
 from component import parameter as cp
@@ -13,8 +22,8 @@ planet = SimpleNamespace()
 
 # parameters
 planet.url = 'https://api.planet.com/auth/v1/experimental/public/my/subscriptions'
-planet.basemaps = "https://tiles.planet.com/basemaps/v1/planet-tiles/{mosaic_name}/gmap/{{z}}/{{x}}/{{y}}.png?api_key={key}&proc={color}"
-planet.attribution = "Imagery © Planet Labs Inc."
+planet.mosaic_name = "planet_medres_normalized_analytic_{}_mosaic"
+planet.data = "Planet MedRes"
 
 # attributes
 
@@ -48,67 +57,22 @@ def validate_key(key, out):
     planet.valid = any([True for sub in subs if sub['state'] == 'active'])
     check_key()
     
+    # autheticate to planet
+    planet.client = api.ClientV1(api_key=key)
+    
     planet.key = key
     
     out.add_msg(cm.planet.valid_key, 'success')
     
-    return 
-
-def order_basemaps(key, out):
-    """check the apy key and then order the basemap to update the select list"""
-    
-    # checking the key validity
-    validate_key(key, out)
-    
-    out.add_msg(cm.planet.load_mosaics)
-    
-    # autheticate to planet
-    planet.client = api.ClientV1(api_key=planet.key)
-    
-    # get the basemap names 
-    mosaics = [m['name'] for m in planet.client.get_mosaics().get()['mosaics']]
-    
-    out.add_msg(cm.planet.mosaic_loaded, 'success')
-    
-    return mosaics
-
-def display_basemap(mosaic_name, m, out, color=None):
-    """display the planet mosaic basemap on the map"""
-    
-    out.add_msg(cm.map.tiles)
-    
-    # set the color if necessary 
-    if not color:
-        color = cp.planet_colors[0]
-    
-    # remove the existing layers with planet attribution 
-    for layer in m.layers:
-        if layer.attribution == planet.attribution: 
-            m.remove_layer(layer)
-            
-    # create a new Tile layer on the map 
-    layer = TileLayer(
-        url=planet.basemaps.format(key=planet.key, mosaic_name=mosaic_name, color=color),
-        name="Planet© Mosaic",
-        attribution=planet.attribution,
-        show_loading = True
-    )
-    
-    # insert the mosaic bewteen CardoDB and the country border ie position 1
-    # we have already removed the planet layers so I'm sure that nothing is in 
-    # The grid and the country are build before and if we are here I'm also sure that there are 3 layers in the map
-    tmp_layers = list(m.layers)
-    tmp_layers.insert(1, layer)
-    m.layers = tuple(tmp_layers)
-    
     return
 
-def download_quads(aoi_name, mosaic_name, grid, out):
+def download_quads(filename, year, grid, bands, out):
     """export each quad to the appropriate folder"""
     
     out.add_msg(cm.planet.export)
     
-    # get the mosaic from the mosaic name 
+    # get the mosaic from the mosaic name
+    mosaic_name = planet.mosaic_name.format(cp.planet_date_ranges[year])
     mosaics = planet.client.get_mosaics().get()['mosaics'] 
     mosaic_names = [m['name'] for m in mosaics]
     mosaic = mosaics[mosaic_names.index(mosaic_name)]
@@ -118,24 +82,170 @@ def download_quads(aoi_name, mosaic_name, grid, out):
     for i, row in grid.iterrows():
         quads.append(f'{int(row.x):04d}-{int(row.y):04d}')
         
-    
+    file_list = []
     for quad_id in quads:
         
         # check file existence 
-        res_dir = cp.get_mosaic_dir(aoi_name, mosaic_name)
-        file = res_dir.joinpath(f'{quad_id}.tif')
+        file = cp.tmp_dir.joinpath(f'{filename}_{year}_{quad_id}.tif')
+        file_list.append(str(file))
         
         if file.is_file():
             out.add_msg(cm.planet.file_exist.format(file))
             continue
-            
-        out.add_msg(cm.planet.down_file.format(file))
-        quad = planet.client.get_quad_by_id(mosaic, quad_id).get()
-        planet.client.download_quad(quad).get_body().write(file)
         
-    out.add_msg(cm.planet.down_end.format(res_dir), "success")
+        tmp_file = cp.tmp_dir.joinpath(f'{filename}_{year}_{quad_id}_tmp.tif')
+        out.add_msg(cm.planet.down_file.format(tmp_file))
+        quad = planet.client.get_quad_by_id(mosaic, quad_id).get()
+        planet.client.download_quad(quad).get_body().write(tmp_file)
+        
+        with rio.open(tmp_file) as src:
+            
+            # adapt the file to only keep the 3 required bands
+            data = src.read(cp.planet_bands_combo[bands])
+            
+            # reproject the image in EPSG:4326
+            dst_crs = 'EPSG:4326'
+            transform, width, height = calculate_default_transform(
+                src.crs, 
+                dst_crs, 
+                src.width, 
+                src.height, 
+                *src.bounds
+            )
+            
+            kwargs = src.meta.copy()
+            kwargs.update({
+                'count': 3,
+                'crs': dst_crs,
+                'transform': transform,
+                'width': width,
+                'height': height
+            })
+            
+            with rio.open(file, 'w', **kwargs) as dst:
+                dst.write(data)
+        
+        #remove the tmp file
+        tmp_file.unlink()
     
-    return
+    return file_list
+
+def get_planet_vrt(pts, start, end, square_size, file, bands, out):
+    
+    # get the filename
+    filename = Path(file).stem
+    
+    # create a range_year element to simplyfy next for loops
+    range_year = [y for y in range(start, end + 1)]
+    
+    # create the buffer grid 
+    gdf_buffers = pts.to_crs("EPSG:3857")
+    gdf_buffers['geometry'] = gdf_buffers.buffer(square_size/2, cap_style = 3)
+    gdf_buffers = gdf_buffers.to_crs("EPSG:4326") 
+    
+    # find all the quads that should be downloaded and serve them as a grid 
+    planet_grid = get_planet_grid(gdf_buffers['geometry'], out)
+    
+    # create a vrt for each year 
+    vrt_list = {}
+    total_img = 
+    for i, year in enumerate(range_year):
+        
+        # download the requested images 
+        file_list = download_quads(filename, year, planet_grid, bands, out, i)
+        
+        # create a vrt out of it 
+        vrt_path = cp.tmp_dir.joinpath(f'{filename}_{year}.vrt')
+        ds = gdal.BuildVRT(str(vrt_path), file_list)
+        ds.FlushCache()
+        
+        # check that the file was effectively created (gdal doesn't raise errors)
+        if not vrt_path.is_file():
+            raise Exception(f"the vrt {vrt_path} was not created")
+        
+        vrt_list[year] = vrt_path
+        
+        out.update_progress(i/len(range_year), msg='Image loaded')
+        
+    # create a title list to be consistent
+    title_list = {y: planet.data for y in range_year}
+    
+    return vrt_list, title_list
+        
+def get_planet_grid(squares, out):
+    """create a grid adapted to the points and to the planet initial grid"""
+    
+    out.add_msg(cm.planet.grid)
+    
+    # get the shape of the aoi in EPSG:3857 proj 
+    aoi_shp = unary_union(squares)
+    aoi_gdf = gpd.GeoDataFrame({'geometry': [aoi_shp]}, crs="EPSG:4326").to_crs('EPSG:3857')
+        
+    # extract the aoi shape 
+    aoi_shp_proj = aoi_gdf['geometry'][0]
+    
+    # retreive the bb 
+    aoi_bb = sg.box(*aoi_gdf.total_bounds)
+        
+    # compute the longitude and latitude in the apropriate CRS
+    crs_4326 = CRS.from_epsg(4326)
+    crs_3857 = CRS.from_epsg(3857)
+    crs_bounds = crs_3857.area_of_use.bounds
+
+    proj = Transformer.from_crs(4326, 3857, always_xy=True)
+    bl = proj.transform(crs_bounds[0], crs_bounds[1])
+    tr = proj.transform(crs_bounds[2], crs_bounds[3])
+
+    # the planet grid is constructing a 2048x2048 grid of SQUARES. 
+    # The latitude extends is bigger (20048966.10m VS 20026376.39) so to ensure the "squariness"
+    # Planet lab have based the numerotation and extends of it square grid on the longitude only. 
+    # the extreme -90 and +90 band it thus exlucded but there are no forest there so we don't care
+    longitudes = np.linspace(bl[0], tr[0], 2048+1)
+
+    # the planet grid size cut the world in 248 squares vertically and horizontally
+    box_size = (tr[0]-bl[0])/2048
+
+    # filter with the geometry bounds
+    bb = aoi_gdf.total_bounds
+
+    # filter lon and lat 
+    lon_filter = longitudes[(longitudes > (bb[0] - box_size)) & (longitudes < bb[2] + box_size)]
+    lat_filter = longitudes[(longitudes > (bb[1] - box_size)) & (longitudes < bb[3] + box_size)]
+
+    # get the index offset 
+    x_offset = np.nonzero(longitudes == lon_filter[0])[0][0]
+    y_offset = np.nonzero(longitudes == lat_filter[0])[0][0]
+    
+    # create the grid
+    x = []
+    y = []
+    names = []
+    squares = []
+    for coords in product(range(len(lon_filter)-1), range(len(lat_filter)-1)):
+    
+        # get the x and y index 
+        ix = coords[0]
+        iy = coords[1]
+        
+        # fill the grid values 
+        x.append(ix + x_offset)
+        y.append(iy + y_offset)
+        names.append(f'L15-{x[-1]:4d}E-{y[-1]:4d}N.tif')
+        squares.append(sg.box(lon_filter[ix], lat_filter[iy], lon_filter[ix+1], lat_filter[iy+1]))
+    
+    # create a buffer grid in 3857
+    grid = gpd.GeoDataFrame({'x':x, 'y':y, 'names':names, 'geometry':squares}, crs='EPSG:3857')
+
+    # cut the grid to the aoi extends 
+    mask = grid.intersects(aoi_shp_proj)
+    grid = grid.loc[mask]
+    
+    # project back to 4326
+    grid_gdf = grid.to_crs('EPSG:4326')
+    
+    return grid_gdf
+    
+    
     
     
     
