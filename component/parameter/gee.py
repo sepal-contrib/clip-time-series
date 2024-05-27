49e1c45ac509f25e7f7ba83ca0b9c13fda0d305c
dfguerrerom <dfgm2006@gmail.com>
2024-05-27 18:28:18 +0200

    feat: fix gee sources

--- component/parameter/gee.py
+++ component/parameter/gee.py
@@ -1,9 +1,10 @@
 import datetime
 from collections import OrderedDict
+from sepal_ui.scripts.utils import init_ee
 
 import ee
 
-ee.Initialize()
+init_ee()
 
 # data parameters
 gee_min_landsat_year = 1985  # launch of landsat 5
@@ -11,21 +12,23 @@ gee_min_sentinel_year = 2015  # launch of sentinel 2
 gee_max_end_year = datetime.datetime.now().year
 sources = ["landsat", "sentinel"]
 
+
 # functions to access parameters according to the used satellite
 def getSatellites(sources, year):
 
     satellites = OrderedDict()
-
+    # https://developers.google.com/earth-engine/landsat_c1_to_c2#oli
     if "sentinel" in sources and year >= 2015:
-        # cannot use SR as they don't cover years before 2020
-        satellites["sentinel_2"] = "COPERNICUS/S2"
+        satellites["sentinel_2"] = "COPERNICUS/S2_HARMONIZED"
     if "landsat" in sources:
+        if year >= 2022:
+            satellites["landsat_9"] = "LANDSAT/LC09/C02/T1_L2"
         if year >= 2013:
             satellites["landsat_8"] = "LANDSAT/LC08/C02/T1_L2"
         if year <= 2013:
-            satellites["landsat_5"] = "LANDSAT/LT05/C01/T1_SR"
+            satellites["landsat_5"] = "LANDSAT/LT05/C02/T1_L2"
         if year >= 1999:
-            satellites["landsat_7"] = "LANDSAT/LE07/C01/T1_SR"
+            satellites["landsat_7"] = "LANDSAT/LE07/C02/T1_L2"
 
     return satellites
 
@@ -36,6 +39,7 @@ def getShortname(satellite):
         "landsat_5": "L5",
         "landsat_7": "L7",
         "landsat_8": "L8",
+        "landsat_9": "L9",
     }
 
     # in parralel sometime the code doesn't manage to write the appropriate key.
@@ -47,7 +51,13 @@ def getShortname(satellite):
 
 def getScale(satellite):
 
-    scale = {"sentinel_2": 10, "landsat_5": 30, "landsat_7": 30, "landsat_8": 30}
+    scale = {
+        "sentinel_2": 10,
+        "landsat_5": 30,
+        "landsat_7": 30,
+        "landsat_8": 30,
+        "landsat_9": 30,
+    }
 
     return scale[satellite]
 
@@ -62,51 +72,59 @@ def getAvailableBands():
     """
     bands = {
         "Red, Green, Blue": {
-            "landsat_7": ["B3", "B2", "B1"],
-            "landsat_5": ["B3", "B2", "B1"],
+            "landsat_7": ["SR_B3", "SR_B2", "SR_B1"],
+            "landsat_5": ["SR_B3", "SR_B2", "SR_B1"],
             "landsat_8": ["SR_B4", "SR_B3", "SR_B2"],
+            "landsat_9": ["SR_B4", "SR_B3", "SR_B2"],
             "sentinel_2": ["B4", "B3", "B2"],
         },
         "Nir, Red, Green": {
-            "landsat_7": ["B4", "B3", "B2"],
-            "landsat_5": ["B4", "B3", "B2"],
+            "landsat_7": ["SR_B4", "SR_B3", "SR_B2"],
+            "landsat_5": ["SR_B4", "SR_B3", "SR_B2"],
             "landsat_8": ["SR_B5", "SR_B4", "SR_B3"],
+            "landsat_9": ["SR_B5", "SR_B4", "SR_B3"],
             "sentinel_2": ["B8", "B4", "B3"],
         },
         "Nir, Swir1, Red": {
-            "landsat_7": ["B4", "B5", "B3"],
-            "landsat_5": ["B4", "B5", "B3"],
+            "landsat_7": ["SR_B4", "SR_B5", "SR_B3"],
+            "landsat_5": ["SR_B4", "SR_B5", "SR_B3"],
             "landsat_8": ["SR_B5", "SR_B6", "SR_B4"],
+            "landsat_9": ["SR_B5", "SR_B6", "SR_B4"],
             "sentinel_2": ["B8", "B11", "B4"],
         },
         "Swir2, Nir, Red": {
-            "landsat_7": ["B7", "B4", "B3"],
-            "landsat_5": ["B7", "B4", "B3"],
+            "landsat_7": ["SR_B7", "SR_B4", "SR_B3"],
+            "landsat_5": ["SR_B7", "SR_B4", "SR_B3"],
             "landsat_8": ["SR_B7", "SR_B5", "SR_B4"],
+            "landsat_9": ["SR_B7", "SR_B5", "SR_B4"],
             "sentinel_2": ["B12", "B8", "B4"],
         },
         "Swir2, Swir1, Red": {
-            "landsat_7": ["B7", "B5", "B3"],
-            "landsat_5": ["B7", "B5", "B3"],
+            "landsat_7": ["SR_B7", "SR_B5", "SR_B3"],
+            "landsat_5": ["SR_B7", "SR_B5", "SR_B3"],
             "landsat_8": ["SR_B7", "SR_B6", "SR_B4"],
+            "landsat_9": ["SR_B7", "SR_B6", "SR_B4"],
             "sentinel_2": ["B12", "B11", "B4"],
         },
         "Swir2, Nir, Green": {
-            "landsat_7": ["B7", "B4", "B2"],
-            "landsat_5": ["B7", "B4", "B2"],
+            "landsat_7": ["SR_B7", "SR_B4", "SR_B2"],
+            "landsat_5": ["SR_B7", "SR_B4", "SR_B2"],
             "landsat_8": ["SR_B7", "SR_B5", "SR_B3"],
+            "landsat_9": ["SR_B7", "SR_B5", "SR_B3"],
             "sentinel_2": ["B12", "B8", "B3"],
         },
         "ndvi": {  # 2 useful bands nir and red
-            "landsat_7": ["B4", "B3"],
-            "landsat_5": ["B4", "B3"],
+            "landsat_7": ["SR_B4", "SR_B3"],
+            "landsat_5": ["SR_B4", "SR_B3"],
             "landsat_8": ["SR_B5", "SR_B4"],
+            "landsat_9": ["SR_B5", "SR_B4"],
             "sentinel_2": ["B8", "B4"],
         },
         "ndwi": {  # 2 useful bands nir and swir
-            "landsat_7": ["B4", "B5"],
-            "landsat_5": ["B4", "B5"],
+            "landsat_7": ["SR_B4", "SR_B5"],
+            "landsat_5": ["SR_B4", "SR_B5"],
             "landsat_8": ["SR_B5", "SR_B6"],
+            "landsat_9": ["SR_B5", "SR_B6"],
             "sentinel_2": ["B8", "B11"],
         },
     }
@@ -119,7 +137,7 @@ def getCloudMask(satelliteId):
     if satelliteId in ["landsat_5", "landsat_7"]:
 
         def cloudMask(image):
-            qa = image.select("pixel_qa")
+            qa = image.select("QA_PIXEL")
             # If the cloud bit (5) is set and the cloud confidence (7) is high
             # or the cloud shadow bit is set (3), then it's a bad pixel.
             cloud = qa.bitwiseAnd(1 << 5).Or(qa.bitwiseAnd(1 << 3))
@@ -129,7 +147,7 @@ def getCloudMask(satelliteId):
 
             return image.updateMask(cloud.Not()).updateMask(mask2)
 
-    elif satelliteId == "landsat_8":
+    elif satelliteId in ["landsat_8", "landsat_9"]:
 
         def cloudMask(image):
             # Bits 3 and 5 are cloud shadow and cloud, respectively.
--- component/scripts/gee.py
+++ component/scripts/gee.py
@@ -1,6 +1,8 @@
 import threading
+import concurrent.futures
+
+from typing import List, Literal, Tuple
 import zipfile
-from concurrent import futures
 from functools import partial
 from pathlib import Path
 from urllib.request import urlretrieve
@@ -8,6 +10,7 @@ from urllib.request import urlretrieve
 import ee
 from osgeo import gdal
 from sepal_ui import sepalwidgets as sw
+from sepal_ui.scripts.utils import init_ee
 
 from component import parameter as cp
 from component import widget as cw
@@ -15,76 +18,112 @@ from component.message import cm
 
 from .utils import min_diagonal
 
-ee.Initialize()
+init_ee()
 
 
-def getImage(sources, bands, mask, year):
+def get_ee_image(
+    satellites: dict,
+    satellite_id: Literal["sentinel_2", "landsat_5", "landsat_7", "landsat_8"],
+    start: str,
+    end: str,
+    bands: str,
+    aoi: ee.geometry.Geometry,
+) -> Tuple[ee.ImageCollection, ee.Image]:
 
+    # create the feature collection name
+    dataset = (
+        ee.ImageCollection(satellites[satellite_id])
+        .filterDate(start, end)
+        .filterBounds(aoi)
+        .map(cp.getCloudMask(satellite_id))
+    )
+
+    ee_image = (
+        dataset.median().clip(aoi).select(cp.getAvailableBands()[bands][satellite_id])
+    )
+
+    return dataset, ee_image
+
+
+def visible_pixel(ee_image: ee.Image, aoi: ee.geometry.Geometry, scale: int) -> float:
+    """get the proportion of visible pixel in the image."""
+
+    # get the number of masked pixel
+    pixel_masked = (
+        ee_image.select(0)
+        .reduceRegion(
+            reducer=ee.Reducer.count(),
+            geometry=aoi,
+            scale=scale,
+        )
+        .values()
+        .get(0)
+    )
+
+    # get the number of pixel in the image
+    pixel = (
+        ee_image.select(0)
+        .unmask()
+        .reduceRegion(
+            reducer=ee.Reducer.count(),
+            geometry=aoi,
+            scale=scale,
+        )
+        .values()
+        .get(0)
+    )
+
+    return ee.Number(pixel_masked).divide(ee.Number(pixel)).multiply(100).getInfo()
+
+
+def getImage(
+    sources: Literal["sentinel", "landsat"],
+    bands: str,
+    aoi: ee.geometry.Geometry,
+    year: int,
+):
+    # print("get_image", sources, bands, aoi, year)
     start = str(year) + "-01-01"
     end = str(year) + "-12-31"
 
     # priority selector for satellites
     satellites = cp.getSatellites(sources, year)
-    for satelliteId in satellites:
 
-        # create the feature collection name
-        dataset = (
-            ee.ImageCollection(satellites[satelliteId])
-            .filterDate(start, end)
-            .filterBounds(mask)
-            .map(cp.getCloudMask(satelliteId))
+    for satellite_id in satellites:
+
+        dataset, ee_image = get_ee_image(
+            satellites, satellite_id, start, end, bands, aoi
         )
 
-        clip = (
-            dataset.median()
-            .clip(mask)
-            .select(cp.getAvailableBands()[bands][satelliteId])
-        )
+        scale = cp.getScale(satellite_id)
 
         visible = 0
         if dataset.size().getInfo():
-            # retreive the name of the first band
-            band = cp.getAvailableBands()[bands][satelliteId][0]
-            scale = cp.getScale(satelliteId)
 
-            # get the number of masked pixel
-            pixel_masked = (
-                clip.select(band)
-                .reduceRegion(
-                    reducer=ee.Reducer.count(),
-                    geometry=mask,
-                    scale=scale,
-                )
-                .get(band)
-            )
-
-            # get the number of pixel in the image
-            pixel = (
-                clip.select(band)
-                .unmask()
-                .reduceRegion(reducer=ee.Reducer.count(), geometry=mask, scale=scale)
-                .get(band)
-            )
-
-            # proportion of masked pixels
-            visible = (
-                ee.Number(pixel_masked).divide(ee.Number(pixel)).multiply(100).getInfo()
-            )
+            # get the proportion of visible pixel
+            visible = visible_pixel(ee_image, aoi, scale)
+            # print(visible, satellite_id)
 
         # if its the last one I'll keep it anyway
         if visible > 50:
             break
 
-    return (clip, satelliteId)
+    return (ee_image, satellite_id)
 
 
-def get_gee_vrt(geometry, mosaics, size, file, bands, sources, output: cw.CustomAlert):
+def get_gee_vrt(
+    geometry,
+    mosaics,
+    size,
+    filename: str,
+    bands: str,
+    sources,
+    output: cw.CustomAlert,
+    tmp_dir: Path,
+):
 
     #  guess if the geometry is only points
     all([r.geometry.geom_type == "Point" for _, r in geometry.iterrows()])
-
-    # get the filename
-    filename = Path(file).stem
 
     # create the points list
     ee_pts = [ee.Geometry.Point(*g.centroid.coords) for g in geometry.geometry]
@@ -101,16 +140,15 @@ def get_gee_vrt(geometry, mosaics, size, file, bands, sources, output: cw.Custom
     # create a filename list
     descriptions = {}
     for year in mosaics:
-        descriptions[year] = f"{filename}_{name_bands}_{year}"
-
+        descriptions[year] = f"{filename}_{year}"
     # load the data directly in SEPAL
     satellites = {}  # contain the names of the used satellites
 
-    nb_points = max(1, len(ee_buffers) - 1)
+    nb_points = max(1, len(ee_buffers))
     total_images = len(mosaics) * nb_points
-    output.reset_progress(total_images, "Image loaded")
-    for year in mosaics:
+    output.reset_progress(total_images, "Progress")
 
+    for year in mosaics:
         satellites[year] = [None] * len(ee_buffers)
 
         download_params = {
@@ -122,14 +160,28 @@ def get_gee_vrt(geometry, mosaics, size, file, bands, sources, output: cw.Custom
             "output": output,
             "satellites": satellites,
             "lock": threading.Lock(),
+            "tmp_dir": tmp_dir,
         }
 
         # for buffer in ee_buffers:
         #    down_buffer(buffer, **download_params)
 
         # download the images in parralel fashion
-        with futures.ThreadPoolExecutor() as executor:  # use all the available CPU/GPU
-            executor.map(partial(down_buffer, **download_params), ee_buffers)
+        with concurrent.futures.ThreadPoolExecutor() as executor:  # use all the available CPU/GPU
+            # executor.map(partial(down_buffer, **download_params), ee_buffers)
+
+            futures = {
+                executor.submit(
+                    partial(down_buffer, **download_params), ee_buffer
+                ): ee_buffer
+                for ee_buffer in ee_buffers
+            }
+
+            # Check if any future has raised an exception
+            for future in concurrent.futures.as_completed(futures):
+                e = future.exception()
+                if e:
+                    raise e  # Rethrow the first exception encountered
 
     # print(satellites)
 
@@ -138,8 +190,8 @@ def get_gee_vrt(geometry, mosaics, size, file, bands, sources, output: cw.Custom
     for year in mosaics:
 
         # retreive the file names
-        vrt_path = cp.tmp_dir.joinpath(f"{descriptions[year]}.vrt")
-        filepaths = [str(f) for f in cp.tmp_dir.glob(f"{descriptions[year]}_*.tif")]
+        vrt_path = tmp_dir / f"{descriptions[year]}.vrt"
+        filepaths = [str(f) for f in tmp_dir.glob(f"{descriptions[year]}_*.tif")]
 
         # build the vrt
         ds = gdal.BuildVRT(str(vrt_path), filepaths)
@@ -177,6 +229,7 @@ def down_buffer(
     descriptions,
     output: sw.Alert,
     satellites,
+    tmp_dir: str,
     lock=None,
 ):
     """download the image for a specific buffer."""
@@ -195,7 +248,7 @@ def down_buffer(
 
     description = f"{descriptions[year]}_{j}"
 
-    dst = cp.tmp_dir / f"{description}.tif"
+    dst = tmp_dir / f"{description}.tif"
 
     if not dst.is_file():
 
@@ -210,7 +263,7 @@ def down_buffer(
             }
         )
 
-        tmp = cp.tmp_dir.joinpath(f"{name}.zip")
+        tmp = tmp_dir.joinpath(f"{name}.zip")
         urlretrieve(link, tmp)
 
         # unzip the file
